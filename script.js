ad2b18e8660484858465ec881061270941d55e0e
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-25 14:13:31 +0530

    Random Number Guesser Game

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let userNumber = 0;
-let guessed = 0;
+let guessCounter = 0;
 let randomNumber = Math.floor(Math.random()*10 + 1);
+
+
 document.getElementById("submitButton").onclick = function(){
+   
    userNumber = document.getElementById("userNumber").value;
-   let message = userNumber == randomNumber ? 'You guessed it right' : 'You guessed it wrong';
-   console.log(message);
-  
-   document.getElementById("message").innerHTML = message + `It you ${guessed} attempts`; 
-   guessed++;
+   userNumber = Number(userNumber);
 
-
+   if(userNumber === randomNumber ){
+      document.getElementById("message").innerHTML = `You guessed the Correct Number <br> It took you ${guessCounter+1} times to guess`;
+   }
+   else if(userNumber < randomNumber){
+      document.getElementById("message").innerHTML = `You guessed a smaller #`;
+   }
+   else if(userNumber > randomNumber){
+      document.getElementById("message").innerHTML = `You guessed a larger #`;
+   }
+   guessCounter++;
 }
