2746d1c73a291e9c9077f27d357499ec2a8df73f
Sanyam Jain <Sanyam0029@gmail.com>
2023-04-25 10:31:29 +0530

    # guessing game

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let userNumber = 0;
 let guessed = 0;
-let randonNumber = Math.floor(Math.random()*10 + 1);
+let randomNumber = Math.floor(Math.random()*10 + 1);
 document.getElementById("submitButton").onclick = function(){
    userNumber = document.getElementById("userNumber").value;
-   let message = userNumber == randonNumber ? 'You guessed it right' : 'You guessed it wrong';
+   let message = userNumber == randomNumber ? 'You guessed it right' : 'You guessed it wrong';
    console.log(message);
   
-   document.getElementById("message").innerHTML = message; 
+   document.getElementById("message").innerHTML = message + `It you ${guessed} attempts`; 
    guessed++;
-   console.log(randonNumber);
+
+
 }
